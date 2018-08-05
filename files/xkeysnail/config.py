e04f5463408f670a54ac7153a89f32056f0ce286
tokibi <tatu014@gmail.com>
2018-08-05 12:11:40 +0900

    キーマップ修正

--- files/xkeysnail/config.py
+++ files/xkeysnail/config.py
@@ -1,42 +1,41 @@
 import re
 from xkeysnail.transform import *
 
+define_modmap({
+    Key.LEFT_ALT: Key.LEFT_CTRL,
+    Key.RIGHT_ALT: Key.LEFT_CTRL,
+})
 
 # Oneshot modifier
 define_multipurpose_modmap({
-    Key.SPACE: [Key.SPACE, Key.LEFT_SHIFT],
-    Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL],
+    Key.SPACE:    [Key.SPACE,    Key.LEFT_SHIFT],
+    Key.CAPSLOCK: [Key.ESC,      Key.LEFT_ALT],
+    Key.MUHENKAN: [Key.MUHENKAN, Key.LEFT_CTRL],
+    Key.HENKAN:   [Key.HENKAN,   Key.LEFT_CTRL],
 })
 
 # [Global keymap]
 define_keymap(None, {
     # Vim lie key bindings
-    K("C-h"): K("left"),
-    K("C-l"): K("right"),
-    K("C-k"): K("up"),
-    K("C-j"): K("down"),
-    K("Shift-C-h"): K("Shift-left"),
-    K("Shift-C-l"): K("Shift-right"),
-    K("Shift-C-k"): K("Shift-up"),
-    K("Shift-C-j"): K("Shift-down"),
+    K("M-h"): K("left"),
+    K("M-l"): K("right"),
+    K("M-k"): K("up"),
+    K("M-j"): K("down"),
+    K("Shift-M-h"): K("Shift-left"),
+    K("Shift-M-l"): K("Shift-right"),
+    K("Shift-M-k"): K("Shift-up"),
+    K("Shift-M-j"): K("Shift-down"),
 
     # Globalize Home, End shortcuts
-    K("C-a"): K("home"),
-    K("C-e"): K("end"),
-    K("Shift-C-a"): K("Shift-home"),
-    K("Shift-C-e"): K("Shift-end"),
-
-    # Select all
-    K("M-a"): K("C-a"),
+    K("M-a"): K("home"),
+    K("M-e"): K("end"),
+    K("Shift-M-a"): K("Shift-home"),
+    K("Shift-M-e"): K("Shift-end"),
 
     # Exchange underscore and backslash
     K("RO"): K("Shift-RO"),
     K("Shift-RO"): K("RO"),
 
     # Delete
-    K("C-d"): K("delete"),
+    K("M-d"): K("delete"),
 }, "cursor move")
-
-define_keymap(re.compile("Code"), {
-    K("M-d"): K("C-d"),
-}, "Visutal Studio Code")
